bfbcb68f6c5f467541eed8819eec2595be4b3a93
Maciej Jagiello <maciek.jagiello@gmail.com>
2018-11-18 21:46:45 +0100

    Better list equals

--- brute.py
+++ brute.py
@@ -1,13 +1,6 @@
-'''
-
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
-# Hello World program in Python
-
 from copy import deepcopy
+import time
+from itertools import combinations_with_replacement
 
 goal = [[1,2,3],
         [4,5,6],
@@ -30,13 +23,6 @@ def op_2(state):
 def op_3(state):
     return op(state, 1, 1)
 
-def op(state, x_offset, y_offset):
-    changed = deepcopy(state)
-    changed[0+y_offset][0+x_offset] = state[1+y_offset][0+x_offset]
-    changed[0+y_offset][1+x_offset] = state[0+y_offset][0+x_offset]
-    changed[1+y_offset][1+x_offset] = state[0+y_offset][1+x_offset]
-    changed[1+y_offset][0+x_offset] = state[1+y_offset][1+x_offset]
-    return changed
 
 def op(state, x_offset, y_offset):
     state[0+y_offset][0+x_offset], state[0+y_offset][1+x_offset], state[1+y_offset][1+x_offset], state[1+y_offset][0+x_offset]\
@@ -48,8 +34,7 @@ ops = {0: op_0,
        1: op_1,
        2: op_2,
        3: op_3}
-import time
-from itertools import combinations_with_replacement
+
 def brute():
     for l in range(0, 40):
         print(l, time.time())
@@ -61,7 +46,7 @@ def brute():
                 curr_table = ops[step](curr_table)
                 # print ("step", step)
                 # print (curr_table)
-                if list(map(tuple, goal)) == list(map(tuple, curr_table)):
+                if goal == curr_table:
                     print ("yeah", sequence)
                     return sequence
 print(brute())
