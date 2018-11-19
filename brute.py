c4e0d2a5a6cb48c73c57b3b5c04e659f6432149c
Maciej Jagiello <maciek.jagiello@gmail.com>
2018-11-19 09:14:03 +0100

    Refactor to classes

--- brute.py
+++ brute.py
@@ -16,78 +16,95 @@ curr = [[4,1,3],
         [5,6,9],
         [7,2,8]]
 
-curr = [[1,2,3],
-        [4,5,9],
-        [7,8,6]]
+# curr = [[1,2,3],
+#         [4,5,9],
+#         [7,8,6]]
 
-def op_0(state):
-    return op(state, 0, 0)
-def op_1(state):
-    return op(state, 1, 0)
-def op_2(state):
-    return op(state, 1, 1)
-def op_3(state):
-    return op(state, 1, 1)
+class Game():
 
+    def __init__(self, init_state):
+        self.ops = {0: self.op_0,
+                    1: self.op_1,
+                    2: self.op_2,
+                    3: self.op_3}
+        self.state = self.mycopy(init_state)
+        self.goal_state = [[1,2,3], [4,5,6], [7,8,9]]
 
-def op(state, x_offset, y_offset):
-    state[0+y_offset][0+x_offset], state[0+y_offset][1+x_offset], state[1+y_offset][1+x_offset], state[1+y_offset][0+x_offset]\
-    = state[1+y_offset][0+x_offset], state[0+y_offset][0+x_offset], state[0+y_offset][1+x_offset], state[1+y_offset][1+x_offset]
+    def __str__(self):
+        return '\n'.join([str(line) for line in self.state])
 
-    return state
+    def op_0(self):
+        return self._op(0, 0)
+    def op_1(self):
+        return self._op(1, 0)
+    def op_2(self):
+        return self._op(1, 1)
+    def op_3(self):
+        return self._op(1, 1)
 
-ops = {0: op_0,
-       1: op_1,
-       2: op_2,
-       3: op_3}
+    def _op(self, x_offset, y_offset):
+        self.state[0+y_offset][0+x_offset], self.state[0+y_offset][1+x_offset], self.state[1+y_offset][1+x_offset], self.state[1+y_offset][0+x_offset]\
+        = self.state[1+y_offset][0+x_offset], self.state[0+y_offset][0+x_offset], self.state[0+y_offset][1+x_offset], self.state[1+y_offset][1+x_offset]
 
+        return self.state
 
-def mycopy(lst):
-    return list(map(list, lst))
+    @staticmethod
+    def mycopy(lst):
+        return list(map(list, lst))
+
+    def completed(self):
+        return self.goal_state == self.state
+
+    def move(self, move):
+        self.ops[move]()
+
 
 a = [[1,2],]
-b = mycopy(a)
+b = Game.mycopy(a)
 b[0][0] = 4
 assert a != b
 
 
 def test():
+
+    def pprint_game(g):
+        print(g)
+        print ("---")
+
+    g = Game([[1,2,3],
+             [4,5,9],
+             [7,8,6]])
+
     s = (0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 2, 0, 2)
 
-    def pprint(obj):
-        for elem in obj:
-            print(elem)
-        print ("---")
-    t = curr
-
     for ss in s:
-        pprint(t)
+        pprint_game(g)
         print(ss)
-        t = mycopy(t)
-        t = ops[ss](t)
+        g.move(ss)
 
-    pprint(t)
+    pprint_game(g)
 
-    raise Exception()
+    if not g.completed():
+        raise Exception()
 
 
-# test()
+test()
 
 def do(l):
     print("start", l)
     prev_time = time.time()
     for sequence in itertools.product(range(4), repeat=l):
-        curr_table = mycopy(curr)
+        game = Game(curr)
         for i, step in enumerate(sequence):
-            curr_table = ops[step](curr_table)
-            if goal == curr_table:
+            game.move(step)
+            if game.completed():
                 print (l, "yeah", sequence[:i+1], time.time() - prev_time)
                 return sequence[:i+1]
     print("done", l, time.time() - prev_time)
 
 def brute():
     with Pool(3) as p:
-        return p.map(do, range(14,15))
+        return p.map(do, range(10))
 
 res = brute()
 
