11ecf2932cde68e7502eec8653eb75305f05dc15
Maciej Jagiello <maciek.jagiello@gmail.com>
2018-11-25 14:36:56 +0100

    Add tests

--- brute.py
+++ brute.py
@@ -17,85 +17,58 @@ curr = [[4,1,3],
         [5,6,9],
         [7,2,8]]
 
-# curr = [[1,2,3],
-#         [4,5,9],
-#         [7,8,6]]
+curr = [[1,2,3],
+        [4,5,9],
+        [7,8,6]]
 
 class Game():
 
     def __init__(self, init_state):
-        self.ops = {0: self.op_0,
-                    1: self.op_1,
-                    2: self.op_2,
-                    3: self.op_3}
-        self.state = self.mycopy(init_state)
-        self.goal_state = [[1,2,3], [4,5,6], [7,8,9]]
+        self.ops = {0: lambda: self._op(0, 0),
+                    1: lambda: self._op(1, 0),
+                    2: lambda: self._op(0, 1),
+                    3: lambda: self._op(1, 1)}
+        self._state = init_state
+        self._goal_state = [[1,2,3], [4,5,6], [7,8,9]]
 
     def __str__(self):
-        return '\n'.join([str(line) for line in self.state])
+        return '\n'.join([str(line) for line in self._state])
 
-    def op_0(self):
-        return self._op(0, 0)
-    def op_1(self):
-        return self._op(1, 0)
-    def op_2(self):
-        return self._op(1, 1)
-    def op_3(self):
-        return self._op(1, 1)
+    def __eq__(self, other):
+        return self._state == other._state
 
     def _op(self, x_offset, y_offset):
-        self.state[0+y_offset][0+x_offset], self.state[0+y_offset][1+x_offset], self.state[1+y_offset][1+x_offset], self.state[1+y_offset][0+x_offset]\
-        = self.state[1+y_offset][0+x_offset], self.state[0+y_offset][0+x_offset], self.state[0+y_offset][1+x_offset], self.state[1+y_offset][1+x_offset]
+        self._state[0+y_offset][0+x_offset], \
+        self._state[0+y_offset][1+x_offset], \
+        self._state[1+y_offset][1+x_offset], \
+        self._state[1+y_offset][0+x_offset]\
+        = self._state[1+y_offset][0+x_offset], \
+        self._state[0+y_offset][0+x_offset], \
+        self._state[0+y_offset][1+x_offset], \
+        self._state[1+y_offset][1+x_offset]
 
-        return self.state
+        return self._state
 
     @staticmethod
     def mycopy(lst):
         return list(map(list, lst))
 
+    def clone(self):
+        return Game(list(map(list, self._state)))
+
     def completed(self):
-        return self.goal_state == self.state
+        return self._goal_state == self._state
 
     def move(self, move):
         self.ops[move]()
 
-
-a = [[1,2],]
-b = Game.mycopy(a)
-b[0][0] = 4
-assert a != b
-
-
-def test():
-
-    def pprint_game(g):
-        print(g)
-        print ("---")
-
-    g = Game([[1,2,3],
-             [4,5,9],
-             [7,8,6]])
-
-    s = (0, 0, 0, 2, 0, 2, 0, 0, 0, 2, 2, 0, 2)
-
-    for ss in s:
-        pprint_game(g)
-        print(ss)
-        g.move(ss)
-
-    pprint_game(g)
-
-    if not g.completed():
-        raise Exception()
-
-
-test()
 
 def do(start_state, l):
     print("start", l)
     prev_time = time.time()
     for sequence in itertools.product(range(4), repeat=l):
         game = Game(start_state)
+        game = game.clone()
         for i, step in enumerate(sequence):
             game.move(step)
             if game.completed():
@@ -103,18 +76,20 @@ def do(start_state, l):
                 return sequence[:i+1]
     print("done", l, time.time() - prev_time)
 
-def brute(curr):
-    do_f = functools.partial(do, curr)
+def brute(state):
+    do_f = functools.partial(do, state)
     with Pool(3) as p:
         return p.map(do_f, range(10))
 
-res = brute(curr)
+if __name__ == '__main__':
 
-res_without_none = [r for r in res if r is not None]
+    res = brute(curr)
 
-if not res_without_none:
-    print("no answer")
-else:
-    shortest_len = min(map(len, res_without_none))
-    shortest_sequences = [r for r in res_without_none if len(r) == shortest_len]
-    print ("best answer", set(shortest_sequences))
+    res_without_none = [r for r in res if r is not None]
+
+    if not res_without_none:
+        print("no answer")
+    else:
+        shortest_len = min(map(len, res_without_none))
+        shortest_sequences = [r for r in res_without_none if len(r) == shortest_len]
+        print ("best answer", set(shortest_sequences))
---
+++ tests.py
@@ -0,0 +1,69 @@
+import unittest
+from brute import Game
+
+class MoveTest(unittest.TestCase):
+  def test_completed(self):
+    state = [[1,2,3],
+             [4,5,6],
+             [7,8,9]]
+    game = Game(state)
+    self.assertTrue(game.completed())
+
+  def test_first_move(self):
+    state = [[2,5,3],
+             [1,4,6],
+             [7,8,9]]
+    game = Game(state)
+    game.move(0)
+    self.assertTrue(game.completed())
+
+  def test_second_move(self):
+    state = [[1,3,6],
+             [4,2,5],
+             [7,8,9]]
+    game = Game(state)
+    game.move(1)
+    self.assertTrue(game.completed())
+
+  def test_third_move(self):
+    state = [[1,2,3],
+             [5,8,6],
+             [4,7,9]]
+    game = Game(state)
+    game.move(2)
+    self.assertTrue(game.completed())
+
+  def test_fourth_move(self):
+    state = [[1,2,3],
+             [4,6,9],
+             [7,5,8]]
+    game = Game(state)
+    game.move(3)
+    self.assertTrue(game.completed())
+
+  def test_sequence(self):
+    state = [[1,2,3],
+             [4,5,9],
+             [7,8,6]]
+    sequence = (2, 3, 1, 2, 2, 2, 1, 1, 1)
+
+    game = Game(state)
+    for step in sequence:
+        self.assertFalse(game.completed())
+        game.move(step)
+    self.assertTrue(game.completed())
+
+
+class GameTest(unittest.TestCase):
+  def test_clone(self):
+    state = [[1,2,3], [4,5,6], [7,8,9]]
+    game = Game(state)
+
+    game2 = game.clone()
+
+    self.assertEqual(game, game2)
+    game.move(1)
+    self.assertNotEqual(game, game2)
+    game2.move(1)
+    self.assertEqual(game, game2)
+
